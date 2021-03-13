73bd64ff51204f35e9854cd492039883a33af8c6
Jason Verbeek <jason@localhost8080.org>
2021-03-13 19:02:26 +0100

    locate config file by name and run configured editor

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,53 @@
+use serde::{Serialize, Deserialize};
 use std::env;
+use shellexpand::tilde;
 
-const CONFIG_FILES: &'static [&'static [&'static str]] = &[
-    &["qtile", "~/.config/qtile/config.py", "/dev/null"],
-    &["nvim", "~/.config/nvim/init.vim", "/dev/null"],
-    &["alacritty", "~/.config/alacritty/customize.yml", "/dev/null"]
-];
 
-fn find_config(program: &str) {
-    println!("{:#?}", CONFIG_FILES[0][0] == program);
+#[derive(Debug, Serialize, Deserialize)]
+struct ConfigurableItem {
+    pub name: String,
+    pub path: String,
+    pub default_path: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Configuration {
+    pub editor: String,
+    pub config: Vec<ConfigurableItem>
+}
+
+impl ::std::default::Default for Configuration {
+    fn default() -> Self { Self {
+        editor: String::from("vim"),
+        config: vec![
+            ConfigurableItem {
+                name: String::from("nvim"),
+                path: String::from("~/.config/nvim/init.vim"),
+                default_path: String::from("/dev/null"),
+            },
+            ConfigurableItem {
+                name: String::from("vim"),
+                path: String::from("~/.vimrc"),
+                default_path: String::from("/dev/null"),
+            }
+        ]
+    }}
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    find_config("qtile")
+    if args.len() != 2 {
+        println!("Usage: rconf <name>");
+        println!("Add more configs in ~/.config/rconf/rconf.toml");
+        std::process::exit(1);
+    }
+    let target = &args[1];
+    let cfg: Configuration = confy::load("rconf").unwrap();
+    match cfg.config.iter().find(|x| &x.name == target) {
+        Some(result) => {
+            let config_file = tilde(&result.path).into_owned();
+            std::process::Command::new(&cfg.editor).arg(config_file).status().expect("error");
+        },
+        None => println!("config not found for '{}' in ~/.config/rconf/rconf.toml", target),
+    }
 }
