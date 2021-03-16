435c2ea39b7f4258d65cb9beea777525f6c2ba0c
Jason Verbeek <jvb@bigbrother.nl>
2021-03-16 12:27:50 +0100

    use directories instead of directories_next until confy0.5.0

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use serde::{Serialize, Deserialize};
-use std::env;
+use directories::ProjectDirs;
+use serde::{Deserialize, Serialize};
 use shellexpand::tilde;
+use std::env;
 use std::path::Path;
-use directories_next::ProjectDirs;
-
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ConfigurableItem {
@@ -15,7 +14,7 @@ struct ConfigurableItem {
 #[derive(Debug, Serialize, Deserialize)]
 struct Configuration {
     pub editor: String,
-    pub config: Vec<ConfigurableItem>
+    pub config: Vec<ConfigurableItem>,
 }
 
 fn get_config_path() -> String {
@@ -32,26 +31,28 @@ fn get_config_path() -> String {
 }
 
 impl ::std::default::Default for Configuration {
-    fn default() -> Self { Self {
-        editor: String::from("vim"),
-        config: vec![
-            ConfigurableItem {
-                name: String::from("nvim"),
-                path: String::from("~/.config/nvim/init.vim"),
-                default_path: String::from("/dev/null"),
-            },
-            ConfigurableItem {
-                name: String::from("vim"),
-                path: String::from("~/.vimrc"),
-                default_path: String::from("/dev/null"),
-            },
-            ConfigurableItem {
-                name: String::from("rconf"),
-                path: get_config_path(),
-                default_path: String::from("/dev/null"),
-            },
-        ]
-    }}
+    fn default() -> Self {
+        Self {
+            editor: String::from("vim"),
+            config: vec![
+                ConfigurableItem {
+                    name: String::from("nvim"),
+                    path: String::from("~/.config/nvim/init.vim"),
+                    default_path: String::from("/dev/null"),
+                },
+                ConfigurableItem {
+                    name: String::from("vim"),
+                    path: String::from("~/.vimrc"),
+                    default_path: String::from("/dev/null"),
+                },
+                ConfigurableItem {
+                    name: String::from("rconf"),
+                    path: get_config_path(),
+                    default_path: String::from("/dev/null"),
+                },
+            ],
+        }
+    }
 }
 
 fn main() {
@@ -66,14 +67,22 @@ fn main() {
     match cfg.config.iter().find(|x| &x.name == target) {
         Some(result) => {
             let config_file = tilde(&result.path).into_owned();
-            if !Path::new(&config_file).exists() && (result.default_path != "/dev/null" || result.default_path != "") {
+            if !Path::new(&config_file).exists()
+                && (result.default_path != "/dev/null" || result.default_path != "")
+            {
                 match std::fs::copy(&result.default_path, &config_file) {
-                    Ok(_) => {},
-                    Err(_) => {},
+                    Ok(_) => {}
+                    Err(_) => {}
                 }
             }
-            std::process::Command::new(&cfg.editor).arg(config_file).status().expect("error");
-        },
-        None => println!("config not found for '{}' in ~/.config/rconf/rconf.toml", target),
+            std::process::Command::new(&cfg.editor)
+                .arg(config_file)
+                .status()
+                .expect("error");
+        }
+        None => println!(
+            "config not found for '{}' in ~/.config/rconf/rconf.toml",
+            target
+        ),
     }
 }
